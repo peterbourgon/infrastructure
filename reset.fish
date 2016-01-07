a55263d277018ca2a89898b053fb2ea532503f06
Peter Bourgon <peter@bourgon.org>
2016-01-07 14:00:49 +0100

    Heh.

--- reset.fish
+++ reset.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-for host in (cat infrastructure.tf | grep "^module" | grep "_host" | awk '{print $2}' | tr -d '"' | sed -e 's/_host//')
+cat infrastructure.tf | grep "^module" | grep "_host" | awk '{print $2}' | tr -d '"' | sed -e 's/_host//' | while read host
 	sed -i '' "/^$host.*/d" $HOME/.ssh/known_hosts
 end
