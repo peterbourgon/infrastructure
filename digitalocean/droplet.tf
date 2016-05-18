b52d6856acd95bcf437d2e48e7a4016ac714da7a
Peter Bourgon <peter@bourgon.org>
2016-05-18 10:27:40 +0100

    docker-compose 1.6

--- digitalocean/droplet.tf
+++ digitalocean/droplet.tf
@@ -59,7 +59,7 @@ resource "digitalocean_droplet" "droplet" {
 			#"curl -SsL https://get.docker.com/ | sed -e 's/docker-engine/docker-engine=1.8.3-0~jessie/' | sh",
 			"curl -SsL https://get.docker.com/ | sh",
 			"usermod -aG docker ${var.user}",
-			"curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose",
+			"curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose",
 			"chmod +x /usr/local/bin/docker-compose",
 
 			# Weave
