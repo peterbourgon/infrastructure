f7501f7622b2e1a03c2c86df4a7f4c0bba328e55
Peter Bourgon <peter@bourgon.org>
2016-05-18 10:27:58 +0100

    Various update

--- infrastructure.tf
+++ infrastructure.tf
@@ -18,21 +18,21 @@ variable "authorized_keys_file" {}
 
 variable "cloudflare_domain" {}
 
-module "archimedes_host" {
-	source = "./digitalocean"
-	hostname = "archimedes"
-	region = "nyc3"
-	size = "512mb"
-	ssh_fingerprint = "${var.ssh_fingerprint}"
-	ssh_key_file = "${var.ssh_key_file}"
-	authorized_keys_file = "${var.authorized_keys_file}"
-}
-module "archimedes_dns" {
-	source = "./cloudflare"
-	cloudflare_domain = "${var.cloudflare_domain}"
-	cloudflare_name = "archimedes"
-	cloudflare_value = "${module.archimedes_host.ip}"
-}
+#module "archimedes_host" {
+#	source = "./digitalocean"
+#	hostname = "archimedes"
+#	region = "nyc3"
+#	size = "512mb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "archimedes_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "archimedes"
+#	cloudflare_value = "${module.archimedes_host.ip}"
+#}
 
 #module "husserl_host" {
 #	source = "./digitalocean"
@@ -50,67 +50,48 @@ module "archimedes_dns" {
 #	cloudflare_value = "${module.husserl_host.ip}"
 #}
 
-#module "chomsky_host" {
+#module "a_host" {
 #	source = "./digitalocean"
-#	hostname = "chomsky"
-#	region = "nyc3"
+#	hostname = "a"
+#	region = "ams3"
 #	size = "4gb"
 #	ssh_fingerprint = "${var.ssh_fingerprint}"
 #	ssh_key_file = "${var.ssh_key_file}"
 #	authorized_keys_file = "${var.authorized_keys_file}"
 #}
-#module "chomsky_dns" {
+#module "a_dns" {
 #	source = "./cloudflare"
 #	cloudflare_domain = "${var.cloudflare_domain}"
-#	cloudflare_name = "chomsky"
-#	cloudflare_value = "${module.chomsky_host.ip}"
+#	cloudflare_name = "a"
+#	cloudflare_value = "${module.a_host.ip}"
 #}
-
-#module "k0_host" {
+#module "b_host" {
 #	source = "./digitalocean"
-#	hostname = "k0"
+#	hostname = "b"
 #	region = "ams3"
-#	size = "1gb"
+#	size = "4gb"
 #	ssh_fingerprint = "${var.ssh_fingerprint}"
 #	ssh_key_file = "${var.ssh_key_file}"
 #	authorized_keys_file = "${var.authorized_keys_file}"
 #}
-#module "k0_dns" {
+#module "b_dns" {
 #	source = "./cloudflare"
 #	cloudflare_domain = "${var.cloudflare_domain}"
-#	cloudflare_name = "k0"
-#	cloudflare_value = "${module.k0_host.ip}"
+#	cloudflare_name = "b"
+#	cloudflare_value = "${module.b_host.ip}"
 #}
-#
-#module "k1_host" {
+#module "c_host" {
 #	source = "./digitalocean"
-#	hostname = "k1"
-#	region = "nyc3"
-#	size = "1gb"
+#	hostname = "c"
+#	region = "ams3"
+#	size = "4gb"
 #	ssh_fingerprint = "${var.ssh_fingerprint}"
 #	ssh_key_file = "${var.ssh_key_file}"
 #	authorized_keys_file = "${var.authorized_keys_file}"
 #}
-#module "k1_dns" {
+#module "c_dns" {
 #	source = "./cloudflare"
 #	cloudflare_domain = "${var.cloudflare_domain}"
-#	cloudflare_name = "k1"
-#	cloudflare_value = "${module.k1_host.ip}"
+#	cloudflare_name = "c"
+#	cloudflare_value = "${module.c_host.ip}"
 #}
-#
-#module "k2_host" {
-#	source = "./digitalocean"
-#	hostname = "k2"
-#	region = "sfo1"
-#	size = "1gb"
-#	ssh_fingerprint = "${var.ssh_fingerprint}"
-#	ssh_key_file = "${var.ssh_key_file}"
-#	authorized_keys_file = "${var.authorized_keys_file}"
-#}
-#module "k2_dns" {
-#	source = "./cloudflare"
-#	cloudflare_domain = "${var.cloudflare_domain}"
-#	cloudflare_name = "k2"
-#	cloudflare_value = "${module.k2_host.ip}"
-#}
-
