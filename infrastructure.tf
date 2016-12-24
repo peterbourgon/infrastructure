a7f0bea5b8e2dea9984ecdac25d43a7d4c0758d9
Peter Bourgon <peter@bourgon.org>
2016-12-24 18:37:39 +0100

    Various things

--- infrastructure.tf
+++ infrastructure.tf
@@ -96,3 +96,139 @@ module "archimedes_host" {
 #	cloudflare_value = "${module.baz_host.ip}"
 #}
 # https://gist.github.com/peterbourgon/c1841cbed2e4d2db0ff0ae7b4f2cf2da
+
+#module "forward1_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "forward1"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "forward1_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "forward1"
+#	cloudflare_value = "${module.forward1_host.ip}"
+#}
+#module "forward2_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "forward2"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "forward2_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "forward2"
+#	cloudflare_value = "${module.forward2_host.ip}"
+#}
+#module "forward3_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "forward3"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "forward3_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "forward3"
+#	cloudflare_value = "${module.forward3_host.ip}"
+#}
+#module "ingest1_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "ingest1"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "ingest1_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "ingest1"
+#	cloudflare_value = "${module.ingest1_host.ip}"
+#}
+#module "ingest2_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "ingest2"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "ingest2_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "ingest2"
+#	cloudflare_value = "${module.ingest2_host.ip}"
+#}
+#module "ingest3_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "ingest3"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "ingest3_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "ingest3"
+#	cloudflare_value = "${module.ingest3_host.ip}"
+#}
+#module "store1_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "store1"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "store1_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "store1"
+#	cloudflare_value = "${module.store1_host.ip}"
+#}
+#module "store2_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "store2"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "store2_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "store2"
+#	cloudflare_value = "${module.store2_host.ip}"
+#}
+#module "store3_host" {
+#	source = "./digitalocean/bare"
+#	hostname = "store3"
+#	region = "fra1"
+#	size = "1gb"
+#	ssh_fingerprint = "${var.ssh_fingerprint}"
+#	ssh_key_file = "${var.ssh_key_file}"
+#	authorized_keys_file = "${var.authorized_keys_file}"
+#}
+#module "store3_dns" {
+#	source = "./cloudflare"
+#	cloudflare_domain = "${var.cloudflare_domain}"
+#	cloudflare_name = "store3"
+#	cloudflare_value = "${module.store3_host.ip}"
+#}
