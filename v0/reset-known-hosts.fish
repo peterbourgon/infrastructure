79cd721159da116b2ff1ccc46d81143fcc859339
Peter Bourgon <peter@bourgon.org>
2018-01-18 16:35:07 -0700

    Remove old crud

--- v0/reset-known-hosts.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-
-cat infrastructure.tf | grep "^module" | grep "_host" | awk '{print $2}' | tr -d '"' | sed -e 's/_host//' | while read host
-	sed -i '' "/^$host.*/d" $HOME/.ssh/known_hosts
-end
--- v0/start-consul.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env fish
-
-docker run -d --name=consul --net=host gliderlabs/consul-server -bootstrap -advertise=(ifconfig|grep '10\.'|grep inet|cut -d':' -f2|awk '{print $1}')
