a21446fef49c931f9dea217fdc7f5fb272770e6e
Peter Bourgon <peter@bourgon.org>
2018-04-06 14:24:02 -0700

    Continued

--- nextgen/tf/nextgen.tf
+++ nextgen/tf/nextgen.tf
@@ -7,27 +7,47 @@ provider "google" {
   project     = "professor-wiggles"
 }
 
-#resource "google_compute_instance" "default" {
-#  name         = "microweb"
-#  machine_type = "f1-micro"
-#  zone         = "us-west1-b"
-#
-#  boot_disk {
-#    initialize_params {
-#      image = "debian-cloud/debian-9"
-#    }
-#  }
-#
-#  network_interface {
-#    network = "default"
-#
-#    access_config {
-#      # Specifying this block grants an ephemeral IP
-#    }
-#  }
-#}
+resource "google_compute_instance" "microweb" {
+  name         = "microweb"
+  machine_type = "f1-micro"
+  zone         = "us-west1-b"
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Specifying this block grants an ephemeral IP
+    }
+  }
+}
 
 # gcloud compute config-ssh
 # edit an/inventory.ini an/*.yml
 
+data "template_file" "inventory" {
+  template = "${file("${path.module}/inventory.template")}"
+
+  depends_on = [
+    "google_compute_instance.microweb",
+  ]
+
+  vars {
+    microweb = "microweb ansible_host=${google_compute_instance.microweb.name}.${google_compute_instance.microweb.zone}.${google_compute_instance.microweb.project}"
+  }
+}
+
+resource "null_resource" "inventory" {
+  triggers {
+    template_rendered = "${ data.template_file.inventory.rendered }"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${ data.template_file.inventory.rendered }' > ../an/inventory.ini"
+  }
+}
