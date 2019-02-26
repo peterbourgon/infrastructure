d154b7debcd988de566c7c524df35315815ee6de
Peter Bourgon <peter@bourgon.org>
2019-02-25 19:31:13 -0800

    Forward the agent for git clones

--- provision_base.fish
+++ provision_base.fish
@@ -7,8 +7,8 @@ ssh root@$host -- ./root.sh
 ssh root@$host -- chsh -s /usr/bin/fish pb # extra-sure
 
 scp user1.sh user2.sh pb@$host:
-ssh pb@$host -- ./user1.sh
-ssh pb@$host -- ./user2.sh
+ssh -A pb@$host -- ./user1.sh
+ssh -A pb@$host -- ./user2.sh
 
 echo remember to disable root login
 
