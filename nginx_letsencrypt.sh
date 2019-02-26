ed905485911e2eb1fcbdae8d289a3f0a66b78e6b
Peter Bourgon <peter@bourgon.org>
2019-02-26 15:05:13 -0800

    Slightly nicer nginx_letsencrypt script

--- nginx_letsencrypt.sh
+++ nginx_letsencrypt.sh
@@ -8,10 +8,15 @@ echo 'deb http://deb.debian.org/debian stretch-backports main' > /etc/apt/source
 apt-get update
 apt-get install -y --force-yes certbot python-certbot-nginx -t stretch-backports
 
+echo
 echo point initial DNS records to this box and press enter when complete
+echo
 read x
 certbot --nginx
+echo add more domains later via
+echo
+echo "  sudo certbot --nginx -d sub.example.com,www.example.com,example.com"
+echo
 
-echo to add more domains later, sudo certbot --nginx -d sub.example.com,www.example.com,example.com
 echo cleaning up
 rm nginx_letsencrypt.sh
